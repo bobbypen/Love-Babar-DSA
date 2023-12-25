75a9876a64509e7562e4c8bb89f3a34b84f1f169
bobbypen <bobbyyadav2018@gmail.com>
2023-12-25 23:49:24 +0530

    25-12-23 easy question

--- src/Arrays/Subarraywith0sum.java
+++ src/Arrays/Subarraywith0sum.java
@@ -1,9 +1,35 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class Subarraywith0sum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		int n = 5;
+		int[] arr = {4,2,-3,1,6};
+		
+		for (int i = 0; i < n; i++) {
+			
+			int prev = 0;
+    		for(int j =i;j<arr.length;j++){
+			prev = prev + arr[j];
+			
+			if (prev==0) {
+			
+				System.out.println(true);
+				return;
+			}
+			
+    		}
+			
+		
+		
+	
+	}
+		
+		System.out.println("False");
 
 	}
 
